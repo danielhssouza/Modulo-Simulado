38dbbe59f0ede379cd8f806163e6ada5c9e7c432
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2023-03-25 00:34:59 -0300

    Implementa GetManyByRules para Questoes

--- src/routes/questao.routes.ts
+++ src/routes/questao.routes.ts
@@ -3,16 +3,14 @@ import QuestaoController from '../controllers/QuestaoController'
 
 const routes = Router()
 
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
 routes.get('/questao', QuestaoController.getall)
 
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
 routes.get('/questao/:id', QuestaoController.getById)
 
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
 routes.post('/questao', QuestaoController.post)
 
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
 routes.delete('/questao/:id', QuestaoController.delete)
+
+routes.get('/questao/rules/:id', QuestaoController.GetManyByRules)
 
 export default routes
