8335355cc9751240b08de0abda8eccbae85e2d33
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2023-03-25 00:35:49 -0300

    Refatora IServiceBase

--- src/services/Contracts/IServiceBase.ts
+++ src/services/Contracts/IServiceBase.ts
@@ -1,6 +1,6 @@
-export default interface IServiceBase<T> {
-  /**
-   * Add
-   */
-  Add: (item: T) => Promise<T>
+export interface IServiceBase<D, DTO> {
+  Add: (item: DTO) => Promise<D>
+  GetById: (id: string) => Promise<D | null>
+  GetAll: () => Promise<D[] | null>
+  Delete: (id: string) => Promise<boolean>
 }
